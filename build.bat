b1dcbff8c876a8700742d0316c92845094f77fe1
Okami Chen <sync402@gmail.com>
2024-09-03 10:22:47 +0800

    * upgrade

--- build.bat
+++ build.bat
@@ -4,14 +4,12 @@ set GOOS=linux
 set GOARCH=amd64
 go build -o linux .
 
-@REM
 @REM set CGO_ENABLED=0
 @REM set GOOS=windows
 @REM set GOARCH=amd64
 @REM go build -o win .
 @REM
-@REM @REM
-@REM @REM set CGO_ENABLED=0
-@REM @REM set GOOS=darwin
-@REM @REM set GOARCH=amd64
-@REM @REM go build -o darwin .
+@REM set CGO_ENABLED=0
+@REM set GOOS=darwin
+@REM set GOARCH=amd64
+@REM go build -o darwin .
---
+++ packages/clash/http/controllers/hub_controller.go
@@ -0,0 +1,66 @@
+package controllers
+
+import (
+	"encoding/json"
+	"github.com/Dreamacro/clash/adapter"
+	"github.com/gin-gonic/gin"
+	"github.com/goravel/framework/contracts/http"
+	"github.com/goravel/framework/facades"
+	"goravel/app/models"
+	"goravel/pkg/healthcheck"
+	"time"
+)
+
+type HubController struct {
+	//Dependent services
+}
+
+func NewHubController() *HubController {
+	return &HubController{
+		//Inject services
+	}
+}
+
+func (r *HubController) Index(ctx http.Context) http.Response {
+	request := ctx.Request()
+	id := request.Input("id")
+	if id == "" {
+		return ctx.Response().Json(500, gin.H{
+			"message": " error",
+		})
+	}
+
+	key := request.Input("key")
+	if key == "" {
+		key = "Fae9gi8a"
+	}
+	var obj models.Proxy
+
+	query := facades.Orm().WithContext(ctx).Query()
+	query.WithTrashed().Where("id = ?", id).Find(&obj)
+
+	if obj.Id == 0 {
+		return ctx.Response().Json(500, gin.H{
+			"message": "Not Found",
+		})
+	}
+	var p map[string]interface{}
+	json.Unmarshal([]byte(obj.Body), &p)
+	px, err := adapter.ParseProxy(p)
+	if err != nil {
+		return ctx.Response().Json(500, gin.H{
+			"message": err.Error(),
+		})
+	}
+
+	url := "https://v2.api.iphub.info/guest/ip/" + *obj.Ip + "?c=" + key
+	body, err := healthcheck.HTTPGetBodyViaProxyWithTime(px, url, time.Second*5)
+	if err != nil {
+		return ctx.Response().Json(500, gin.H{
+			"message": err.Error(),
+		})
+	}
+
+	return ctx.Response().Data(http.StatusOK, "application/json;charset=utf-8", body)
+
+}
