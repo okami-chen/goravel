bf7c7550e744fc5fedeb909ca976d8592c674726
okamichen <webmaster@88188.live>
2024-10-10 13:40:49 +0800

    * init

--- packages/clash/commands/request_command.go
+++ packages/clash/commands/request_command.go
@@ -32,24 +32,27 @@ func (receiver *RequestCommand) Extend() command.Extend {
 	return command.Extend{}
 }
 
-func DialContext(uri string) (net.Conn, error) {
+func dialContext(uri string, ctx context.Context) (net.Conn, error) {
 	filePath := facades.Storage().Path("all.yaml")
 	p := tool.RandomProxySimple(filePath)
 	clash, _ := adapter.ParseProxy(p)
 	addr, _ := healthcheck.UrlToMetadata(uri)
-	return clash.DialContext(context.TODO(), &addr)
+	return clash.DialContext(ctx, &addr)
 }
 
 // Handle Execute the console command.
-func (receiver *RequestCommand) Handle(ctx console.Context) error {
+func (receiver *RequestCommand) Handle(cx console.Context) error {
 	uri := "https://www.futbin.com/25/player/49675/asier-villalibre-molina"
 	//uri = "http://ip-api.com/json"
+	uri = "https://tls.peet.ws/api/all"
+	uri = "https://tls.browserleaks.com/json"
+	//uri = "https://ascii2d.net/"
 	client := req.C()
 	client.DialContext = func(ctx context.Context, network, addr string) (net.Conn, error) {
-		return DialContext(uri)
+		return dialContext(uri, ctx)
 	}
 	request := client.DevMode().
-		SetUserAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36").
+		SetUserAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36").
 		SetTLSFingerprintChrome(). // 模拟 Chrome 浏览器的 TLS 握手指纹，让网站相信这是 Chrome 浏览器在访问，予以通行。
 		ImpersonateChrome().
 		EnableForceHTTP2().
@@ -61,7 +64,7 @@ func (receiver *RequestCommand) Handle(ctx console.Context) error {
 		SetCommonRetryHook(func(resp *req.Response, err error) {
 			c := client.Clone()
 			c.DialContext = func(ctx context.Context, network, addr string) (net.Conn, error) {
-				return DialContext(uri)
+				return dialContext(uri, ctx)
 			}
 			resp.Request.SetClient(c) // Change the client of request dynamically.
 		}).
